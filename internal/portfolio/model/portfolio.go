41fd0eb9d0b04181dadb122a8c21285528c917ab
AswinPuthukaatil <aswin100396@gmail.com>
2025-04-24 22:40:42 +0530

    added index to the freelancer_id tag

--- internal/portfolio/model/portfolio.go
+++ internal/portfolio/model/portfolio.go
@@ -1,14 +1,15 @@
-
 package model
 
 import "time"
 
 type Portfolio struct {
-	ID            string    `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
-	FreelancerID  string    `gorm:"type:uuid;not null"`
-	Title         string
-	Description   string
-	ImageURL      string
-	Link          string
-	CreatedAt     time.Time `gorm:"default:now()"`
+	ID           string    `gorm:"primaryKey;type:uuid;default:gen_random_uuid()" json:"id"`
+	FreelancerID string    `gorm:"type:uuid;not null;index" json:"freelancer_id"`
+
+	Title       string `gorm:"type:varchar(255);not null" json:"title"`
+	Description string `gorm:"type:text" json:"description"`
+	ImageURL    string `gorm:"type:text" json:"image_url"`
+	Link        string `gorm:"type:text" json:"link"`
+
+	CreatedAt time.Time `gorm:"default:now()" json:"created_at"`
 }
