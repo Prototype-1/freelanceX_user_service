41d79add1a744155834de319231821a61be69c88
AswinPuthukaatil <aswin100396@gmail.com>
2025-04-25 18:30:11 +0530

    Refactored review service to only clients can make the reviews

--- internal/review/service/review.go
+++ internal/review/service/review.go
@@ -2,9 +2,10 @@ package service
 
 import (
 	"time"
-
+	"errors"
 	"github.com/Prototype-1/freelanceX_user_service/internal/review/model"
 	"github.com/Prototype-1/freelanceX_user_service/internal/review/repository"
+	authRepo "github.com/Prototype-1/freelanceX_user_service/internal/auth/repository"
 )
 
 type ReviewService interface {
@@ -14,19 +15,34 @@ type ReviewService interface {
 
 type reviewService struct {
 	repo repository.ReviewRepository
+	userRepo authRepo.UserRepository
 }
 
-func NewReviewService(repo repository.ReviewRepository) ReviewService {
-	return &reviewService{repo}
+func NewReviewService(repo repository.ReviewRepository, userRepo authRepo.UserRepository) ReviewService {
+	return &reviewService{
+		repo : repo,
+		userRepo: userRepo,
+	}
 }
+
+var ErrUnauthorizedReview = errors.New("only clients are allowed to submit reviews")
 
 func (s *reviewService) SubmitReview(review *model.FreelancerReview) (*model.FreelancerReview, error) {
+	user, err := s.userRepo.GetUserByID(nil, review.ClientID)
+	if err != nil {
+		return nil, err
+	}
+	if user == nil || user.Role != "client" {
+		return nil, ErrUnauthorizedReview 
+	}
+
 	review.CreatedAt = time.Now()
 	if err := s.repo.Create(review); err != nil {
 		return nil, err
 	}
 	return review, nil
 }
+
 
 func (s *reviewService) GetReviewsByFreelancerID(freelancerID string) ([]model.FreelancerReview, error) {
 	return s.repo.GetByFreelancerID(freelancerID)
